102db78564a8167b2c30114f8c40488893fa8a49
SeokJeong0411 <dk04188@naver.com>
2024-02-27 20:03:45 +0900

    240227 Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,15 +1,15 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation, useMotionValueEvent, useScroll } from "framer-motion";
 import styled from "styled-components";
 import { Link, useMatch } from "react-router-dom";
+import { useState } from "react";
 
-const Nav = styled.nav`
+const Nav = styled(motion.nav)`
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100%;
   top: 0;
-  background-color: black;
   font-size: 14px;
   padding: 20px 60px;
   color: white;
@@ -49,14 +49,7 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
-  color: white;
-  svg {
-    height: 25px;
-  }
-`;
-
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   width: 5px;
   height: 5px;
@@ -66,6 +59,29 @@ const Circle = styled.span`
   right: 0;
   margin: 0 auto;
   background-color: ${(props) => props.theme.red};
+`;
+
+const Search = styled.span`
+  color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  svg {
+    height: 25px;
+  }
+`;
+
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 1px solid ${(props) => props.theme.white.lighter};
 `;
 
 const logoVariants = {
@@ -80,14 +96,48 @@ const logoVariants = {
   },
 };
 
+const navVariants = {
+  top: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+  },
+};
+
 function Header() {
   const homeMatch = useMatch("/");
   const tvMatch = useMatch("/tv");
 
-  console.log(homeMatch, tvMatch);
+  const inputAnimation = useAnimation();
+  const navAnimation = useAnimation();
+
+  const { scrollY } = useScroll();
+
+  const [searchOpen, setSearchOpen] = useState(false);
+  const openSearch = () => {
+    if (searchOpen) {
+      inputAnimation.start({
+        scaleX: 0,
+      });
+    } else {
+      inputAnimation.start({
+        scaleX: 1,
+      });
+    }
+    setSearchOpen((prev) => !prev);
+  };
+
+  useMotionValueEvent(scrollY, "change", (scrollY) => {
+    if (scrollY > 80) {
+      navAnimation.start("scroll");
+    } else {
+      navAnimation.start("top");
+    }
+  });
 
   return (
-    <Nav>
+    <Nav variants={navVariants} animate={navAnimation} initial="top">
       <Col>
         <Logo
           variants={logoVariants}
@@ -104,26 +154,39 @@ function Header() {
           <Link to={"/"}>
             <Item>
               <span>Home</span>
-              {homeMatch ? <Circle /> : null}
+              {homeMatch ? <Circle layoutId="circle" /> : null}
             </Item>
           </Link>
           <Link to={"/tv"}>
             <Item>
               Tv Shows
-              {tvMatch ? <Circle /> : null}
+              {tvMatch ? <Circle layoutId="circle" /> : null}
             </Item>
           </Link>
         </Items>
       </Col>
       <Col>
         <Search>
-          <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <motion.svg
+            animate={{ x: searchOpen ? -210 : 0 }}
+            transition={{ type: "linear" }}
+            onClick={openSearch}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               fillRule="evenodd"
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
+          <Input
+            animate={inputAnimation}
+            initial={{ scaleX: 0 }}
+            transition={{ type: "linear" }}
+            placeholder="Search for movie or tv show"
+          />
         </Search>
       </Col>
     </Nav>
